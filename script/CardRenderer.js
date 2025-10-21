04924a6f729c9b7d4f64aab65aee8c1673e02cc1
Marcus3588 <marcee3588@gmail.com>
2025-10-21 02:08:45 +0000

    Added base layout and styling (HTML/CSS; implemented CardRenderer module for rendering cards dynamically

---
+++ script/CardRenderer.js
@@ -0,0 +1,25 @@
+export function renderCard(){
+	const name = document.getElementById('name').value;
+	const title = document.getElementById('title').value;
+	const email = document.getElementById('email').value;
+	const phone = document.getElementById('phone').value;
+	const company = document.getElementById('company').value;
+	const photoInput = document.getElementById('photoUpload').value;
+
+	const card = document.getElementById('card');
+	card.classList.remove('hidden');
+
+	document.getElementById('displayName').textContent = name;
+	document.getElementById('displayTitle').textContent = title;
+	document.getElementById('displayEmail').textContent = email;
+	document.getElementById('displayPhone').textContent = phone;
+	document.getElementById('displayCompany').textContent = company;
+
+	if(photoInput.files[0]) {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			document.getElementById('displayPhoto').src = e.target.result;
+		}
+		reader.readAsDataURL(photoInput.files[0]);
+	}
+}
